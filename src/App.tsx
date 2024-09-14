717578d9e840e21b8a31090cfd3b2399d366fc80
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-14 15:52:32 +0400

    Add: Card HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Card } from "./Components/Card";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Card />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,7 +1,11 @@
 import "./Card.scss";
 
 export function Card() {
-  return <div></div>;
+  return (
+    <div className="card">
+      <h1 className="card-text">Blue</h1>
+    </div>
+  );
 }
 
 export default Card;
