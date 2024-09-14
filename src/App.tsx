93f257b59bf303c13077911d58010bbe47a50b37
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-14 18:53:43 +0400

    Add: Cards Wrapper Style

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Card } from "./Components/Card";
 
 function App() {
   return (
-    <>
+    <div className="card-page-wrapper">
       <Card />
-    </>
+    </div>
   );
 }
 
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -3,7 +3,7 @@ import "./Card.scss";
 export function Card() {
   return (
     <div className="card">
-      <h1 className="card-text">Blue</h1>
+      <p className="card-text">Blue</p>
     </div>
   );
 }
