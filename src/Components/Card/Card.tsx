2d74c311523ce0daf2a4216f20b7fc81efb74cc2
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-15 20:18:24 +0400

    Add: Water filling Effects

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,9 +1,12 @@
+import { Water } from "../Water";
+
 import "./Card.scss";
 
 export function Card({ question }: { question: string }) {
   return (
     <div className="back-pulse">
       <p className="card-text">{question}</p>
+      <Water />
     </div>
   );
 }
---
+++ src/Components/Water/Water.tsx
@@ -0,0 +1,13 @@
+import "./Water.scss";
+
+export function Water() {
+  return (
+    <div className="center">
+      <div className="circle">
+        <div className="wave"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Water;
