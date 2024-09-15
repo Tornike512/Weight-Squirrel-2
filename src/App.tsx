a2d2b6f35603c83625e5eb07626ec857c524fc35
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-15 16:03:01 +0400

    Add: Animated Background

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,34 @@ import { Card } from "./Components/Card";
 
 function App() {
   return (
-    <div className="app">
-      <h1>რომელი გირჩევნია?</h1>
-      <div className="card-page-wrapper">
-        <Card question="აკონტროლო ორი ციყვი" />
-        <Card question="გაიგო ყველაფრის მასა რასაც შეხედავ" />
+    <>
+      <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-    </div>
+      <div className="app">
+        <h1>რომელი გირჩევნია?</h1>
+        <div className="card-page-wrapper">
+          <Card question="აკონტროლო ორი ციყვი" />
+          <Card question="გაიგო ყველაფრის მასა რასაც შეხედავ" />
+        </div>
+      </div>
+    </>
   );
 }
 
