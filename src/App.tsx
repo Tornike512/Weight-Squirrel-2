162ec88613a453ebae281f392ad787e7413dbb97
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-15 19:43:06 +0400

    Add: Votes Style

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,10 @@ function App() {
         <span></span>
       </div>
       <div className="app">
-        <h1>რომელი გირჩევნია?</h1>
+        <h1>
+          რომელი გირჩევნია?
+          <p className="votes">ხმების რაოდენობა:5</p>
+        </h1>
         <div className="card-page-wrapper">
           <Card question="აკონტროლო ორი ციყვი" />
           <Card question="გაიგო ყველაფრის მასა რასაც შეხედავ" />
